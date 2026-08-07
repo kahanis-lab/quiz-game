e30b7e012e9f6938e86b722981d8139f525f38f5
kahanis-lab <kahanis@inha.edu>
2026-08-07 11:38:36 +0900

    try catch

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 name=input("이름이 뭐예요?")
 print(f"{name}님,환영합니다!")
 print("퀴즈 게임을 시작합니다!")
+score=0
 while True:
     print("""
 ========================================
@@ -16,5 +17,33 @@ while True:
     if menu=="5":
         print("게임을 종료합니다.")
         break
+    elif menu == "1":
+        print("""코디세이 오는데 걸리는 시간?
+        1. 10분 
+        2. 30분
+        3. 1시간
+        4. 1시간 30분
+        5. 그 이상""")
+        try:
+           o=int(input("정답 입력:"))
+           if o==4:
+              print("정답입니다")
+              score+=1
+                     
+           else:
+              print("다시 골라")
+        except ValueError:
+            print("숫자만 입력해요.")
+        
+    elif menu =="2":
+        newquiz=input("퀴즈 추가해:")
+        
+    elif menu=="3":
+        print(newquiz)
+    elif menu=="4":
+        print(score)
+    else:
+        print("1~5번까지 고르라니까")
+
 
  
